103ae8ce58cf18889a51b8bbd0d89d558892fc2d
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-06-14 16:36:02 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React, {Component} from 'react';
+
+
+class EyesOnMe extends Component{
+
+    handleFocus = (event) => {
+        console.log("Good!")
+    }
+    handleBlur = (event) => {
+        console.log("Hey! Eyes on me!")
+    }
+    render(){
+        return(
+            <div>
+               <button onFocus={this.handleFocus} onBlur={this.handleBlur}>Hello there!</button>
+            </div>
+        )
+    }
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+import React, {Component} from 'react';
+
+
+class Keypad extends Component{
+
+    handleTypeEvent = (event) => {
+        console.log("Entering password...")
+    }
+    render(){
+        return(
+            <div>
+                <input type="password" onKeyUp={this.handleTypeEvent}/>
+            </div>
+        )
+    }
+}
+export default Keypad;
